08806fbeebcbe8e58f5a7e71ef6f5edcade28024
hilaryml <hilyttle@gmail.com>
2018-05-05 20:59:50 -0400

    add migration files for cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506001554) do
+ActiveRecord::Schema.define(version: 20180506004854) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
