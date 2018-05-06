29622e3ef384a4c87e6a35274e6f903379ebb4e4
hilaryml <hilyttle@gmail.com>
2018-05-05 21:36:57 -0400

    add category and item migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506004854) do
+ActiveRecord::Schema.define(version: 20180506013600) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,22 @@ ActiveRecord::Schema.define(version: 20180506004854) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "inventory"
+    t.decimal  "price"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
