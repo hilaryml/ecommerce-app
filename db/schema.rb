494c8120ca21ce5be77ea7691abed45196887596
hilaryml <hilyttle@gmail.com>
2018-05-05 21:02:50 -0400

    adds default status of active to cart migration model

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20180506004854) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "status",     default: "Active"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
