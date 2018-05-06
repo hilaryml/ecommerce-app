87d76176c2df6657163775f80a48fa2ddfdfd8f2
hilaryml <hilyttle@gmail.com>
2018-05-05 21:04:52 -0400

    spec out cart model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    pending 'requires a user upon creation'
+
+  end
+
+  describe 'relationships' do
+
+    pending 'has many line items that are destroyed upon deletion of cart'
+
+    pending 'has many items through line items'
+
+    pending 'it belongs to a user'
+    
+  end
 end
